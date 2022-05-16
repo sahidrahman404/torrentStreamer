c831bfc5ed95bfe78995f08cffc6fbf9344b57d4
Sahid Rahman <sahidrahman404@gmail.com>
2022-05-16 07:00:24 +0700

    add questions for the user

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 'use strict';
 
 import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+      type: 'list',
+      name: 'category',
+      message: 'Do you wanna watch a movie or a series?',
+      choices: ['Movies', 'TV'],
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What movie do you wanna watch?',
+      validate: answer => {
+        if (answer === '') {
+          return 'please enter a valid title';
+        }
+        return true;
+      },
+    },
+    {
+      type: 'list',
+      name: 'quality',
+      message: 'What resolution do you prefer to stream the video in?',
+      choices: ['480p', '720p', '1080p'],
+      default: '720p',
+    },
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+  });
