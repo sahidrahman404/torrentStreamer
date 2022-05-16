b1bf57686deb9b45ee00dbf9b9dd25e03963fe2e
Sahid Rahman <sahidrahman404@gmail.com>
2022-05-16 07:45:17 +0700

    fixup! fixup! processing the answers

--- index.js
+++ index.js
@@ -2,30 +2,29 @@
 
 import inquirer from 'inquirer';
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-      type: 'list',
-      name: 'category',
-      message: 'Do you wanna watch a movie or a series?',
-      choices: ['Movies', 'TV'],
+const questions = [
+  /* Pass your questions in here */
+  {
+    type: 'list',
+    name: 'category',
+    message: 'Do you wanna watch a movie or a series?',
+    choices: ['Movies', 'TV'],
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What movie do you wanna watch?',
+    validate: answer => {
+      if (answer === '') {
+        return 'please enter a valid title';
+      }
+      return true;
     },
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What movie do you wanna watch?',
-      validate: answer => {
-        if (answer === '') {
-          return 'please enter a valid title';
-        }
-        return true;
-      },
-    },
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-    const title = answers.title.toLowerCase().split(' ').join('%20');
-    const url = `https://1337x.wtf/category-search/${title}/${answers.category}/1/`;
-    console.log(url);
-  });
+  },
+];
+
+inquirer.prompt(questions).then(answers => {
+  // Use user feedback for... whatever!!
+  const title = answers.title.toLowerCase().split(' ').join('%20');
+  const url = `https://1337x.wtf/category-search/${title}/${answers.category}/1/`;
+
