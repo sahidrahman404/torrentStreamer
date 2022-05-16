3f8e31a7c82d00057fd725a7947224d86602e9c0
Sahid Rahman <sahidrahman404@gmail.com>
2022-05-16 07:14:58 +0700

    fixup! processing the answers

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ inquirer
   .prompt([
     /* Pass your questions in here */
     {
+      type: 'list',
+      name: 'category',
+      message: 'Do you wanna watch a movie or a series?',
+      choices: ['Movies', 'TV'],
+    },
+    {
       type: 'input',
       name: 'title',
       message: 'What movie do you wanna watch?',
@@ -20,4 +26,6 @@ inquirer
   .then(answers => {
     // Use user feedback for... whatever!!
     const title = answers.title.toLowerCase().split(' ').join('%20');
+    const url = `https://1337x.wtf/category-search/${title}/${answers.category}/1/`;
+    console.log(url);
   });
